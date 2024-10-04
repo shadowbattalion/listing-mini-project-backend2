b76ebcc14480341489ebeae9b4b44ca19cfe9adb
shadowbattalion <hafizspaceumar@gmail.com>
2024-10-04 22:27:12 +0800

    Changed csv file location

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -5,7 +5,7 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads/')
+      cb(null, 'src/csvParser/')
     },
     filename: function (req, file, cb) {
       
